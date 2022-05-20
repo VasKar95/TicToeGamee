83c038e2eaaf31fad5995e8d6d69bce7e6130d2a
Vaskar95 <vkaramanis1995@gmail.com>
2022-05-20 13:36:28 +0300

    Begin

--- src/TicToeGame.java
+++ src/TicToeGame.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * This is a Tic Toe Game
+ * @author Vasilis Karamanis
+ * */
+
 public class TicToeGame {
-    
+    /**
+     * @param  player1Position represents the position of player1
+     * @param  player2Position represents the position of player2
+     *
+     * */
+    static ArrayList<Integer> player1Position=new ArrayList<Integer>();
+    static ArrayList<Integer>player2Position=new ArrayList<Integer>();
+
+
 }
